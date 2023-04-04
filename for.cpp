a76bcd375ccec10589bf9ef5a29870720e7ccd6b
EwersonNeias <128439222+EwersonNeias@users.noreply.github.com>
2023-04-03 23:30:54 -0300

    Add files via upload

---
+++ for.cpp
@@ -0,0 +1,12 @@
+#include <iostream> 
+using namespace std;
+
+main (void) //informa o tipo do codigo
+{
+    int numero;//para usarmos numeros inteiros
+    
+    for(numero=1; numero<=10; numero++) //aqui o codigo faz o calculo dos numeros 
+    {
+        cout << "Numero ...: " << numero << endl;//aqui o codigo se comunica com o usuario informando a sequencia
+    }
+}
