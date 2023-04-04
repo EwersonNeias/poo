75a7ada375410f5cf08e5adc11e6bfcf3e9b15b7
EwersonNeias <128439222+EwersonNeias@users.noreply.github.com>
2023-04-03 21:31:40 -0300

    Add files via upload

---
+++ operador_relacional.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main() {//int que trabalha com valores inteiros
+    int x = 10;//valor do x
+    int y = 20;//valor do y 
+    
+    if (x > y) {//if faz a diferença de valores x e y
+        cout << "x é maior que y" << endl;//falando com o usuario para informar a diferença de valores
+    }
+    if (x < y) { //if faz a diferença de valores x e y
+        cout << "x é menor que y" << endl;//falando com o usuario para informar a diferença de valores
+    }
+    if (x >= y) {//if faz a diferença de valores x e y
+        cout << "x é maior ou igual a y" << endl;//falando com o usuario para informar a diferença de valores
+    }
+    if (x <= y) {//if faz a diferença de valores x e y
+        cout << "x é menor ou igual a y" << endl;//falando com o usuario para informar a diferença de valores
+    }
+    if (x == y) {//if faz a diferença de valores x e y
+        cout << "x é igual a y" << endl;//falando com o usuario para informar a diferença de valores
+    }
+    return 0;
+}
